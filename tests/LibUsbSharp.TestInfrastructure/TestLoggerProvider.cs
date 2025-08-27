81a38e6d2501758c43f824ea44ae62266c1e04f8
Thomas Mittet <thomas@huddly.com>
2025-08-27 14:48:25 +0200

    update logger provider

--- tests/LibUsbSharp.TestInfrastructure/TestLoggerProvider.cs
+++ tests/LibUsbSharp.TestInfrastructure/TestLoggerProvider.cs
@@ -1,10 +1,19 @@
 namespace LibUsbSharp.TestInfrastructure;
 
-public sealed class TestLoggerProvider(ITestOutputHelper _output) : ILoggerProvider
+public sealed class TestLoggerProvider : ILoggerProvider
 {
+    private readonly ITestOutputHelper _output;
+    private readonly LogLevel _minLevel;
+
+    public TestLoggerProvider(ITestOutputHelper output, LogLevel minLevel = LogLevel.Trace)
+    {
+        _output = output;
+        _minLevel = minLevel;
+    }
+
     public ILogger CreateLogger(string categoryName)
     {
-        return new TestLogger(categoryName, _output);
+        return new TestLogger(categoryName, _output, _minLevel);
     }
 
     public void Dispose() { }
