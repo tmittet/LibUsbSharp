3d2bf0e81b36091c78453eb59cf2903e77a51bfb
Thomas Mittet <thomas@huddly.com>
2025-08-27 13:49:00 +0200

    Add project and test scaffolding for an UVC extension

---
+++ src/LibUsbSharp.Extensions/Uvc/ControlRequestUvc.cs
@@ -0,0 +1,13 @@
+﻿namespace LibUsbSharp.Extensions.Uvc;
+
+public enum ControlRequestUvc : byte
+{
+    SetCurrentSetting = 0x01,
+    GetCurrentSetting = 0x81,
+    GetMinimumValue = 0x82,
+    GetMaximumValue = 0x83,
+    GetResolution = 0x84,
+    GetLength = 0x85,
+    GetInfo = 0x86,
+    GetDefault = 0x87,
+}
---
+++ src/LibUsbSharp.Extensions/Uvc/LibUsbDeviceExtension.cs
@@ -0,0 +1,48 @@
+﻿using LibUsbSharp.Transfer;
+
+namespace LibUsbSharp.Extensions.Uvc;
+
+public static class LibUsbDeviceExtension
+{
+    public static LibUsbResult ControlUvcRead(
+        this IUsbDevice device,
+        ControlRequestRecipient recipient,
+        ControlRequestUvc request,
+        ushort value,
+        ushort index,
+        Span<byte> destination,
+        out ushort bytesRead,
+        int timeout = Timeout.Infinite
+    ) =>
+        device.ControlRead(
+            recipient,
+            ControlRequestType.Class,
+            (byte)request,
+            value,
+            index,
+            destination,
+            out bytesRead,
+            timeout
+        );
+
+    public static LibUsbResult ControlUvcWrite(
+        this IUsbDevice device,
+        ControlRequestRecipient recipient,
+        ControlRequestUvc request,
+        ushort value,
+        ushort index,
+        ReadOnlySpan<byte> source,
+        out int bytesWritten,
+        int timeout = Timeout.Infinite
+    ) =>
+        device.ControlWrite(
+            recipient,
+            ControlRequestType.Class,
+            (byte)request,
+            value,
+            index,
+            source,
+            out bytesWritten,
+            timeout
+        );
+}
