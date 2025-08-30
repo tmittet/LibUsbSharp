d3ab78823102a32057a86b28df6c81a795209065
Thomas Mittet <thomas@huddly.com>
2025-08-30 19:50:19 +0200

    Remove ControlRequestStandard extension
    from LibUsbSharp core project

--- tests/LibUsbSharp.Tests/Given_any_USB_device.cs
+++ tests/LibUsbSharp.Tests/Given_any_USB_device.cs
@@ -123,7 +123,8 @@ public sealed class Given_any_USB_device : IDisposable
     [SkippableFact]
     public void ControlRead_returns_expected_descriptor_given_correct_Device_GetDescriptor_params()
     {
-        const byte DescriptorTypeDevice = 0x01;
+        const byte GetDescriptorRequest = 0x06;
+        const byte DeviceDescriptorType = 0x01;
         const ushort DescriptorIndex = 0x00;
 
         using var device = _deviceSource.OpenUsbDeviceOrSkip();
@@ -134,8 +135,9 @@ public sealed class Given_any_USB_device : IDisposable
 
         var result = device.ControlRead(
             ControlRequestRecipient.Device,
-            ControlRequestStandard.GetDescriptor,
-            (DescriptorTypeDevice << 8) | DescriptorIndex,
+            ControlRequestType.Standard,
+            GetDescriptorRequest,
+            (DeviceDescriptorType << 8) | DescriptorIndex,
             DescriptorIndex,
             descriptorBuffer,
             out var bytesRead
@@ -159,6 +161,8 @@ public sealed class Given_any_USB_device : IDisposable
     [SkippableFact]
     public void ControlWrite_is_successfull_given_params_to_set_current_Configuration()
     {
+        const byte GetConfigurationRequest = 0x08;
+        const byte SetConfigurationRequest = 0x09;
         const ushort DescriptorIndex = 0;
 
         using var device = _deviceSource.OpenUsbDeviceOrSkip();
@@ -167,7 +171,8 @@ public sealed class Given_any_USB_device : IDisposable
         var readBuffer = new byte[1];
         var readResult = device.ControlRead(
             ControlRequestRecipient.Device,
-            ControlRequestStandard.GetConfiguration,
+            ControlRequestType.Standard,
+            GetConfigurationRequest,
             0,
             0,
             readBuffer,
@@ -181,7 +186,8 @@ public sealed class Given_any_USB_device : IDisposable
         // When configuration read is successful, write the same config value back to the device
         var writeResult = device.ControlWrite(
             ControlRequestRecipient.Device,
-            ControlRequestStandard.SetConfiguration,
+            ControlRequestType.Standard,
+            SetConfigurationRequest,
             readBuffer[0],
             DescriptorIndex,
             [],
