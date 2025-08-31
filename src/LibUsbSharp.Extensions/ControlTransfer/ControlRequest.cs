301ec97e6faadb2a727bde1b05f44c505701141e
Thomas Mittet <thomas@huddly.com>
2025-08-31 09:47:45 +0200

    Add an Extensions project and test project
    and implement a ControlTransfer request setup packet factory that
    supports standard control requests.

---
+++ src/LibUsbSharp.Extensions/ControlTransfer/ControlRequest.cs
@@ -0,0 +1,90 @@
+﻿using RequestRecipient = LibUsbSharp.Transfer.ControlRequestRecipient;
+using RequestType = LibUsbSharp.Transfer.ControlRequestType;
+
+namespace LibUsbSharp.Extensions.ControlTransfer;
+
+/// <summary>
+/// A control requst setup packet factory.
+/// </summary>
+public sealed record ControlRequest(
+    RequestRecipient Recipient,
+    RequestType Type,
+    byte Request,
+    ushort Value,
+    ushort Index,
+    ushort Length = 0
+)
+{
+    /// <summary>
+    /// Requests directed to the device.
+    /// </summary>
+    public static class Device
+    {
+        public static ControlRequest Standard(
+            StandardRequest request,
+            ushort value = 0,
+            ushort index = 0,
+            ushort length = 0
+        ) => new(RequestRecipient.Device, RequestType.Standard, (byte)request, value, index, length);
+
+        public static ControlRequest Class(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Device, RequestType.Class, request, value, index, length);
+
+        public static ControlRequest Vendor(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Device, RequestType.Vendor, request, value, index, length);
+    }
+
+    /// <summary>
+    /// Requests directed to the device interface.
+    /// </summary>
+    public static class Interface
+    {
+        public static ControlRequest Standard(
+            StandardRequest request,
+            ushort value = 0,
+            ushort index = 0,
+            ushort length = 0
+        ) => new(RequestRecipient.Interface, RequestType.Standard, (byte)request, value, index, length);
+
+        public static ControlRequest Class(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Interface, RequestType.Class, request, value, index, length);
+
+        public static ControlRequest Vendor(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Interface, RequestType.Vendor, request, value, index, length);
+    }
+
+    /// <summary>
+    /// Requests directed to a specific endpoint on a device.
+    /// </summary>
+    public static class Endpoint
+    {
+        public static ControlRequest Standard(
+            StandardRequest request,
+            ushort value = 0,
+            ushort index = 0,
+            ushort length = 0
+        ) => new(RequestRecipient.Endpoint, RequestType.Standard, (byte)request, value, index, length);
+
+        public static ControlRequest Class(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Endpoint, RequestType.Class, request, value, index, length);
+
+        public static ControlRequest Vendor(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Endpoint, RequestType.Vendor, request, value, index, length);
+    }
+
+    /// <summary>
+    /// Requests directed to "other" elements defined by the class or vendor spec.
+    /// </summary>
+    public static class Other
+    {
+        public static ControlRequest Class(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Other, RequestType.Class, request, value, index, length);
+
+        public static ControlRequest Vendor(byte request, ushort value, ushort index, ushort length = 0) =>
+            new(RequestRecipient.Other, RequestType.Vendor, request, value, index, length);
+    }
+
+    public override string ToString() =>
+        $"{Recipient}/{Type} (0x{(byte)Type << 5 | (byte)Recipient:X2}), "
+        + $"request=0x{Request:X2}, value=0x{Value:X4}, index=0x{Index:X4}, length={Length}";
+}
---
+++ src/LibUsbSharp.Extensions/ControlTransfer/LibUsbDeviceExtension.cs
@@ -0,0 +1,80 @@
+﻿namespace LibUsbSharp.Extensions.ControlTransfer;
+
+public static class LibUsbDeviceExtension
+{
+    /// <summary>
+    /// Send a ControlRead request. Data is read Device -> Host.
+    /// </summary>
+    /// <param name="device">A UsbDevice instance.</param>
+    /// <param name="transfer">The USB standard spec, class spec or vendor defined request</param>
+    /// <param name="destination">A destination span for read bytes</param>
+    /// <param name="bytesRead">The number of bytes read</param>
+    /// <param name="timeout">Timeout before giving up due to no response being received</param>
+    /// <exception cref="ArgumentException">Thrown when the destination buffer is too large.</exception>
+    /// <returns>
+    /// Success = The read operation completed successfully.<br />
+    /// IO = The read operation failed.<br />
+    /// InvalidParameter = Transfer size is larger than OS or hardware can support.<br />
+    /// NoDevice = The device has been disconnected.<br />
+    /// ResourceBusy = Halt condition detected (endpoint stalled) or control request not supported.<br />
+    /// Timeout = The read operation timed out.<br />
+    /// Overflow = The device sent more data than expected.<br />
+    /// Interrupted = The read operation was canceled.<br />
+    /// NotSupported = The transfer flags are not supported by the operating system.<br />
+    /// </returns>
+    public static LibUsbResult ControlRead(
+        this IUsbDevice device,
+        ControlRequest transfer,
+        Span<byte> destination,
+        out ushort bytesRead,
+        int timeout = Timeout.Infinite
+    ) =>
+        device.ControlRead(
+            transfer.Recipient,
+            transfer.Type,
+            transfer.Request,
+            transfer.Value,
+            transfer.Index,
+            destination,
+            out bytesRead,
+            timeout
+        );
+
+    /// <summary>
+    /// Send a ControlWrite request. Data is written Host -> Device.
+    /// </summary>
+    /// <param name="device">A UsbDevice instance.</param>
+    /// <param name="transfer">The USB standard spec, class spec or vendor defined request</param>
+    /// <param name="source">The payload to send to the device (max. 65.535 bytes)</param>
+    /// <param name="timeout">Timeout before giving up due to no response being received</param>
+    /// <param name="bytesWritten">The actual number of bytes written to the device</param>
+    /// <exception cref="ArgumentException">Thrown when the source payload is too large.</exception>
+    /// <returns>
+    /// Success = The write operation completed successfully.<br />
+    /// IO = The write operation failed.<br />
+    /// InvalidParameter = Transfer size is larger than OS or hardware can support.<br />
+    /// NoDevice = The device has been disconnected.<br />
+    /// ResourceBusy = Halt condition detected (endpoint stalled) or control request not supported.<br />
+    /// Timeout = The write operation timed out.<br />
+    /// Overflow = The host sent more data than expected.<br />
+    /// Interrupted = The write operation was canceled.<br />
+    /// NotSupported = The transfer flags are not supported by the operating system.<br />
+    /// </returns>
+    public static LibUsbResult ControlWrite(
+        this IUsbDevice device,
+        ControlRequest transfer,
+        ReadOnlySpan<byte> source,
+        out int bytesWritten,
+        int timeout = Timeout.Infinite
+    ) =>
+        device.ControlWrite(
+            transfer.Recipient,
+            transfer.Type,
+            transfer.Request,
+            transfer.Value,
+            transfer.Index,
+            source,
+            out bytesWritten,
+            timeout
+        );
+}
