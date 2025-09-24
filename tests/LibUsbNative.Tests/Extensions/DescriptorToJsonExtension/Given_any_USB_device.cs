7eef106cb68b120c99ec845798b17b4d17b310d6
Thomas Mittet <thomas@huddly.com>
2025-09-24 16:08:09 +0200

    Refactor: Use shared LibUsbNativeTestBase type
    for DescriptorToJsonExtension and DescriptorToStringExtension tests

---
+++ tests/LibUsbNative.Tests/Extensions/DescriptorToJsonExtension/Given_any_USB_device.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using LibUsbNative.Extensions;
+using LibUsbNative.Structs;
+using LibUsbNative.Tests.Fakes;
+using Xunit.Abstractions;
+
+namespace LibUsbNative.Tests.Extensions.DescriptorToJsonExtension;
+
+public class Given_any_USB_device_Fake(ITestOutputHelper output) : Given_any_USB_device(output, new FakeLibusbApi());
+
+[Trait("Category", "UsbDevice")]
+public class Given_any_USB_device_Real(ITestOutputHelper output) : Given_any_USB_device(output, new PInvokeLibUsbApi());
+
+public abstract class Given_any_USB_device(ITestOutputHelper output, ILibUsbApi api) : LibUsbNativeTestBase(output, api)
+{
+    [Fact]
+    public void UsbConfigDescriptor_serializes_and_deserializes_successfully()
+    {
+        EnterReadLock(() =>
+        {
+            var context = GetContext();
+            var (list, count) = context.GetDeviceList();
+            count.Should().BePositive();
+            var device = list.Devices.ToList()[0];
+            var json = device.GetActiveConfigDescriptor().ToJson();
+            Output.WriteLine(json);
+
+            var deserialized = JsonSerializer.Deserialize<libusb_config_descriptor>(json)!;
+            deserialized.ToJson().Should().Be(json);
+            list.Dispose();
+        });
+    }
+
+    [Fact]
+    public void UsbDeviceDescriptor_serializes_and_deserializes_successfully()
+    {
+        EnterReadLock(() =>
+        {
+            var context = GetContext();
+            var (list, count) = context.GetDeviceList();
+            count.Should().BePositive();
+            var device = list.Devices.ToList()[0];
+            var json = device.GetDeviceDescriptor().ToJson();
+            Output.WriteLine(json);
+
+            var deserialized = JsonSerializer.Deserialize<libusb_device_descriptor>(json)!;
+            deserialized.ToJson().Should().Be(json);
+            list.Dispose();
+        });
+    }
+};
---
+++ tests/LibUsbNative.Tests/Extensions/DescriptorToStringExtension/Given_any_USB_device.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using LibUsbNative.Extensions;
+using LibUsbNative.Tests.Fakes;
+using Xunit.Abstractions;
+
+namespace LibUsbNative.Tests.Extensions.DescriptorToStringExtension;
+
+public class Given_any_USB_device_Fake(ITestOutputHelper output) : Given_any_USB_device(output, new FakeLibusbApi());
+
+[Trait("Category", "UsbDevice")]
+public class Given_any_USB_device_Real(ITestOutputHelper output) : Given_any_USB_device(output, new PInvokeLibUsbApi());
+
+public abstract class Given_any_USB_device(ITestOutputHelper output, ILibUsbApi api) : LibUsbNativeTestBase(output, api)
+{
+    [Fact]
+    public void TestDeviceDescriptorTreeString()
+    {
+        EnterReadLock(() =>
+        {
+            var context = GetContext();
+            var (list, count) = context.GetDeviceList();
+            count.Should().BePositive();
+            var device = list.Devices.ToList()[0];
+            var descriptor = device.GetDeviceDescriptor();
+            Output.WriteLine(descriptor.ToTreeString());
+
+            list.Dispose();
+        });
+    }
+
+    [Fact]
+    public void TestActiveConfigTreeString()
+    {
+        EnterReadLock(() =>
+        {
+            var context = GetContext();
+            var (list, count) = context.GetDeviceList();
+            count.Should().BePositive();
+            var device = list.Devices.ToList()[0];
+            var descriptor = device.GetActiveConfigDescriptor();
+            Output.WriteLine(descriptor.ToTreeString());
+
+            list.Dispose();
+        });
+    }
+};
