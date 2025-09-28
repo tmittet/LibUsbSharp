883a8bb467807e2872bc91ec459bb02f96c7412b
Thomas Mittet <thomas@huddly.com>
2025-09-28 10:24:21 +0200

    Improve ISafeDevice method comments/summaries

--- src/LibUsbSharp.Native/SafeHandles/ISafeDevice.cs
+++ src/LibUsbSharp.Native/SafeHandles/ISafeDevice.cs
@@ -5,41 +5,41 @@ namespace LibUsbSharp.Native.SafeHandles;
 public interface ISafeDevice
 {
     /// <summary>
-    /// Open the USB device.
+    /// Open the USB device. Enables you to perform I/O on the device.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     /// <exception cref="LibUsbException">Thrown when the device open operation fails.</exception>
     ISafeDeviceHandle Open();
 
     /// <summary>
-    /// Get the device descriptor. NOTE: Since libusb-1.0.16, this function always succeeds.
+    /// Get the USB device descriptor. NOTE: Since libusb-1.0.16, this function always succeeds.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     libusb_device_descriptor GetDeviceDescriptor();
 
     /// <summary>
-    /// Get a pointer to the active config descriptor.
+    /// Get the USB configuration descriptor for the currently active configuration.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     /// <exception cref="LibUsbException">Thrown when the get descriptor operation fails.</exception>
     libusb_config_descriptor GetActiveConfigDescriptor();
 
     /// <summary>
-    /// Get a pointer to the active config descriptor.
+    /// Get a pointer to the USB configuration descriptor for the currently active configuration.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     /// <exception cref="LibUsbException">Thrown when the get pointer operation fails.</exception>
     ISafeConfigDescriptorPtr GetActiveConfigDescriptorPtr();
 
     /// <summary>
-    /// Get the config descriptor.
+    /// Get a USB configuration descriptor based on its index.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     /// <exception cref="LibUsbException">Thrown when the get descriptor operation fails.</exception>
     libusb_config_descriptor GetConfigDescriptor(byte configIndex);
 
     /// <summary>
-    /// Get a pointer to the config descriptor.
+    /// Get a pointer to a USB configuration descriptor based on its index.
     /// </summary>
     /// <exception cref="ObjectDisposedException">Thrown when the ISafeDevice is disposed.</exception>
     /// <exception cref="LibUsbException">Thrown when the get pointer operation fails.</exception>
